02fded4d2953659b34e6dd3902c297d149db1410
syaifulprmrdn <syaifulprimordian@gmail.com>
2025-05-22 08:36:24 +0700

    App.py

--- App.py
+++ App.py
@@ -2,3 +2,4 @@ import streamlit as st
 
 st.title("STD!")
 st.write("MATARAM IS RED MATARAM IS BLUE")
+st.image ("https://i.kym-cdn.com/entries/icons/original/000/038/193/hooh_cover.jpg", caption=" JAWA!", use_column_width=True)
