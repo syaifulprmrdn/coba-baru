cbac9798099aca7ef3692a75561708ca8bb0c65a
syaifulprmrdn <syaifulprimordian@gmail.com>
2025-05-22 09:08:09 +0700

    Update and rename 1_ to 1_.py

---
+++ pages/1_/1_.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from sklearn.model_selection train_test_split
+from sklearn.metrics import classification_report, accuracy_score
+
+st.set_page_config(page_title=prediction")
+st.title("prediction")
+st.header("prediction")
