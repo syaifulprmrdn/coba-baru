302c10f661d0830d0791192b47bdf55ad72c2661
syaifulprmrdn <syaifulprimordian@gmail.com>
2025-05-22 09:17:28 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,17 @@
 import streamlit as st
+import pandas as pd
+import pickle
+import joblib
+import seabor as sns
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_title=("iris dashboard app",layout="centered")
+st.sidebar.header("Dashboard")
+
 
 st.title("STD!")
 st.write("MATARAM IS RED MATARAM IS BLUE")
 st.image ("https://i.kym-cdn.com/entries/icons/original/000/038/193/hooh_cover.jpg", caption=" JAWA!", use_column_width=True)
+
+df - pd.read_csv("model/iris.csv")
+st
