2615af8b24a7cc94f3234e3519eb83896d94e751
syaifulprmrdn <syaifulprimordian@gmail.com>
2025-05-23 21:44:17 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,3 @@
-
-
 st.title("STD!")
 st.write("MATARAM IS RED MATARAM IS BLUE")
 st.image ("https://i.kym-cdn.com/entries/icons/original/000/038/193/hooh_cover.jpg", caption=" JAWA!", use_column_width=True)
