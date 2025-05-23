1913bd9bd808d20ea6dafcf109a871850a3c9cc5
syaifulprmrdn <syaifulprimordian@gmail.com>
2025-05-23 22:27:15 +0700

    Update celeng 2.py

--- pages/celeng 2.py
+++ pages/celeng 2.py
@@ -4,9 +4,9 @@ import streamlit as st
 
 
 
-st.set_page_config(page_title="Mancester is blu")
-st.title("Mancester is blu")
-st.header("Mancester is blu")
-st.title("STD!")
-st.write("MATARAM IS RED MATARAM IS BLUE")
-st.image ("https://i.kym-cdn.com/entries/icons/original/000/038/193/hooh_cover.jpg", caption=" JAWA!", use_column_width=True)
+st.set_page_config(page_title="Mancester is blek")
+st.title("Mancester is blek")
+st.header("Mancester is blek")
+st.title("inpo throphy")
+st.write("16")
+st.image ("https://cdns.klimg.com/mav-prod-resized/720x/webp/newsOg/2024/9/30/1727687517187-4ujfdi.jpeg", caption=" hooraumum!", use_column_width=True)
